99c4c9cb853d67b8a8fbe37450bd9cda9f61fb34
NayanCod <kumarnayab6@gmail.com>
2024-10-15 15:49:43 +0530

    online/offline dynamic

--- frontend/src/home/right/Chatuser.jsx
+++ frontend/src/home/right/Chatuser.jsx
@@ -7,14 +7,14 @@ const Chatuser = () => {
   const { selectedConversation } = useConversation();
   const {socket, onlineUsers} = useSocketContext();
   const getOnlineUserStatus = (userId) => {
-    return onlineUsers.includes(userId) ? 'Online' : 'Offline';
+    return onlineUsers.includes(userId) ? 'online' : 'offline';
   }
   // const isOnline = onlineUsers.includes(user._id);
   return (
     <>
       <div className="px-5 pt-5 pb-3 h-[12vh] flex space-x-4 bg-gray-900 hover:bg-gray-600 cursor-pointer duration-200">
         <div>
-          <div className={`avatar online`}>
+          <div className={`avatar ${getOnlineUserStatus(selectedConversation?._id)}`}>
             <div className="w-14 rounded-full">
               <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
             </div>
@@ -22,8 +22,8 @@ const Chatuser = () => {
         </div>
 
         <div>
-          <h1 className="text-xl">{selectedConversation?.name}</h1>
-          <p className="text-sm">{ getOnlineUserStatus(selectedConversation?._id)}</p>
+          <h1 className="text-lg font-semibold">{selectedConversation?.name}</h1>
+          <p className="text-xs">{ getOnlineUserStatus(selectedConversation?._id)}</p>
         </div>
       </div>
     </>
